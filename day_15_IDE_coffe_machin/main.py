98b92542974d14c6e9efd170be06c91296e0ccaf
xcoder94 <o7451155@yandex.com>
2023-01-07 17:15:32 +0500

    07.01.2023

--- day_15_IDE_coffe_machin/main.py
+++ day_15_IDE_coffe_machin/main.py
@@ -30,59 +30,50 @@ def check_recources(client_want, drinks, remaining):
         
 
 # Recieve payments for drinks
-def process_coins(client_want, drinks, remaining):
+def process_coins(client_want, drinks, money):
     drink_cost = drinks[client_want]['cost']
     client_payment = 0
-    quarter = 8 # int(input('How many quarters? (0.25): '))
+    change = 0
+    quarter = int(input('How many quarters? (0.25): '))
     client_payment += QUARTER * quarter
     print(f'Your payment ${client_payment}')
 
-    dimes = 9 # int(input('How many dimes? (0.10): '))
+    dimes = int(input('How many dimes? (0.10): '))
     client_payment += DIMES * dimes
     print(f'Your payment ${client_payment}')
 
-    nickles = 1 # int(input('How many nickles? (0.05): '))
+    nickles = int(input('How many nickles? (0.05): '))
     client_payment += NICKLES * nickles
     print(f'Your payment ${client_payment}')
 
-    penies = 10 # int(input('How many penies? (0.01): '))
+    penies = int(input('How many penies? (0.01): '))
     client_payment += PENIES * penies    
     print(f'Your payment ${client_payment}')
 
     if client_payment >= drink_cost:
         change = client_payment - drink_cost
         print(f'Here is your {client_want} ☕ Enjoy!')
-
-        resepie = drinks[client_want]['ingredients']
-        for i in resepie:
-            if i in remaining:
-                remaining[i] -= i
-
-        # water_remaining -= client_want['ingredients']['water']
-        # milk_remaing -= client_want['ingredients']['milk']
-        # cofee_remaing -= client_want['ingredients']['coffee']
-        money += client_payment
+        money += client_payment - change
         if change > 0:
             print(f'Here is {change} in change')
     else:
-        print('Sorry that\'s not enough money. Moey refunded')
+        print('Sorry that\'s not enough money. Money refunded')
 
 
-process_coins('cappuccino', MENU, resources)
-print_report()
+# process_coins('cappuccino', MENU, resources['money'])
+# print_report()
 
 
-# machine_is_working = True
-# while machine_is_working:
-#     customer_order = input('“What would you like? (espresso/latte/cappuccino): ')
-#     if customer_order == 'espresso':
-#         check_recources(customer_order, MENU, resources)
-#     elif customer_order == 'latte':
-#         check_recources(customer_order, MENU, resources)
-#     elif customer_order == 'cappuccino':
-#         check_recources(customer_order, MENU, resources)
-#     elif customer_order == 'off':
-#         machine_is_working = False
-#     elif customer_order == 'report':
-#         print_report()
+machine_is_working = True
+while machine_is_working:
+    customer_order = input('“What would you like? (espresso/latte/cappuccino): ')
+    if customer_order in MENU:
+        check_recources(customer_order, MENU, resources)
+        process_coins(customer_order, MENU, resources['money'])
+    elif customer_order == 'off':
+        machine_is_working = False
+    elif customer_order == 'report':
+        print_report()
+    else:
+        print('Please enter the correct name')
 
