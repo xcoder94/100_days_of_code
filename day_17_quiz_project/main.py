4957032408d162a08985875953a5e7af16318e96
xcoder94 <o7451155@yandex.com>
2023-02-03 21:48:05 +0500

    day 17 ended 03.02.2023

--- day_17_quiz_project/main.py
+++ day_17_quiz_project/main.py
@@ -6,13 +6,16 @@ from quiz_brain import QuizBrain
 question_bank = []
 
 for question in question_data:
-    question_text = question['text']
-    question_answer = question['answer']
+    question_text = question['question']
+    question_answer = question['correct_answer']
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
 
 
 quiz = QuizBrain(question_bank)
-quiz.next_question()
+while quiz.still_has_question():
+    quiz.next_question()
 
+print('You\'ve complated the quiz')
+print(f'Your final score is {quiz.score}/{quiz.question_number}')
 
