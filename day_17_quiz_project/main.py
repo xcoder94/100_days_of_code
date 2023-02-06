1a92a718e5bb850f734a6e18cee9102e8932b637
xcoder94 <o7451155@yandex.com>
2023-02-06 22:37:47 +0500

    Day 18 satrted 06.02.2023

--- day_17_quiz_project/main.py
+++ day_17_quiz_project/main.py
@@ -15,7 +15,7 @@ for question in question_data:
 quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
     quiz.next_question()
-
+ 
 print('You\'ve complated the quiz')
 print(f'Your final score is {quiz.score}/{quiz.question_number}')
 
