56af51f14271ce880559736e1903b0a413a94749
xcoder94 <o7451155@yandex.com>
2023-02-08 20:29:31 +0500

    08.02.2023 day 18

--- day_18_Turtle&graphical_ui/sandbox.py
+++ day_18_Turtle&graphical_ui/sandbox.py
@@ -1,20 +1,36 @@
 from turtle import Turtle, Screen
 
-
-timmy_the_turtle = Turtle()
-timmy_the_turtle.shape('turtle')
-timmy_the_turtle.color('red')
-timmy_the_turtle.fd(100)
-
-
-
-
-
-
-
-
-
-
 screen = Screen()
 screen.setup(800, 600)
+
+tim = Turtle()
+
+tim.shape('turtle')
+tim.color('red')
+
+# Drawing a square
+# for _ in range(1, 5):
+#     tim.speed(1)
+#     tim.rt(90)
+#     tim.fd(100)
+
+
+# Drawing a dashed line
+
+for _ in range(1, 11):
+    tim.speed(1)
+    tim.fd(10)
+    tim.pu()
+    tim.fd(10)
+    tim.pd()
+
+
+
+
+
+
+
+
+
+
 screen.exitonclick()
