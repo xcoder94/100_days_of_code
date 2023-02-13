902e666816e5e37e35a969565ebc0a50a9fa45c3
xcoder94 <o7451155@yandex.com>
2023-02-13 23:00:37 +0500

    day 18 is going on

--- day_18_Turtle&graphical_ui/sandbox.py
+++ day_18_Turtle&graphical_ui/sandbox.py
@@ -1,12 +1,12 @@
 from turtle import Turtle, Screen
-
+import random
 screen = Screen()
 screen.setup(800, 600)
 
 tim = Turtle()
 
-tim.shape('turtle')
-tim.color('red')
+# tim.shape('arrow')
+# tim.color('red')
 
 # Drawing a square
 # for _ in range(1, 5):
@@ -17,20 +17,46 @@ tim.color('red')
 
 # Drawing a dashed line
 
-for _ in range(1, 11):
-    tim.speed(1)
-    tim.fd(10)
-    tim.pu()
-    tim.fd(10)
-    tim.pd()
+# for _ in range(1, 11):
+#     tim.speed(1)
+#     tim.fd(10)
+#     tim.pu()
+#     tim.fd(10)
+#     tim.pd()
+
+# My coode
+# for i in range(3, 10):
+#     angle = 360 / i
+#     for j in range(i):
+#         tim.fd(100)
+#         tim.right(angle)
+
+# Angela's code start
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed",
+           "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen"]
+
+ 
+# def draw_shape(num_sides):
+#     angle = 360 / num_sides
+#     for _ in range(num_sides):
+#         tim.forward(100)
+#         tim.right(angle)
 
 
+# for shape_side_n in range(3, 10):
+#     tim.color(random.choice(colours))
+#     draw_shape(shape_side_n)
 
+# Angela's code end
 
-
-
-
-
+tim.pensize(10)
+directions = [0, 90, 180, 270]
+for i in range(200):
+    tim.speed(0)
+    tim.color(random.choice(colours))
+    tim.setheading(random.choice(directions))
+    tim.fd(20)
+    
 
 
 screen.exitonclick()
