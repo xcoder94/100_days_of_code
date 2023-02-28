58fd66d0ca43942d2e2f1737cb948e59333d3979
xcoder94 <o7451155@yandex.com>
2023-02-28 18:51:55 +0500

    day 19 started

---
+++ day_19_instances_state_&_Higher_order_func/sandbox.py
@@ -0,0 +1,44 @@
+from turtle import Turtle, Screen
+
+tim = Turtle()
+screen = Screen()
+screen.setup(720, 720)
+
+
+
+position = tim.pos()
+print(position)
+
+def move_forward():
+    tim.forward(10)
+
+
+def move_back():
+    tim.bk(10)
+
+
+def turn_counter_clockwise():
+    tim.lt(10)
+
+
+def turn_clockwise():
+    tim.rt(10)
+
+
+def clean_and_back():
+    screen.clear()
+    tim.setpos(0, 0)
+    tim.showturtle()
+
+
+
+
+
+
+screen.listen()
+screen.onkey(key='w', fun=move_forward)
+screen.onkey(key='s', fun=move_back)
+screen.onkey(key='a', fun=turn_counter_clockwise)
+screen.onkey(key='d', fun=turn_clockwise)
+screen.onkey(key='c', fun=clean_and_back)
+screen.exitonclick()
