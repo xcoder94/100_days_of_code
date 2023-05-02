ab98f8f4fdc353c5735da0d46d6777dc6086b9bd
xcoder94 <o7451155@yandex.com>
2023-05-02 17:50:14 +0500

    Day 22 startaed at 02.05.2023

---
+++ day_22_pong_game/main.py
@@ -0,0 +1,42 @@
+from turtle import Turtle, Screen
+from paddle import Paddle
+from ball import Ball
+import time
+
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor('black')
+screen.title('Pong game')
+screen.tracer(0)
+
+r_paddle = Paddle((370, 0))
+l_paddle = Paddle((-370, 0))
+ball = Ball()
+
+
+screen.listen()
+screen.onkey(r_paddle.go_up, 'Up')
+screen.onkey(r_paddle.go_down, 'Down')
+screen.onkey(l_paddle.go_up, 'w')
+screen.onkey(l_paddle.go_down, 's')
+
+
+game_is_on = True
+while game_is_on:
+    time.sleep(0.1)
+    screen.update()
+    ball.move()
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce()
+    
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
