a4ad49d92156740bfa1ab544432c91654fbddc9b
xcoder94 <o7451155@yandex.com>
2023-05-31 22:34:49 +0500

    day 27 not ended

--- 27_day_tkinter_*args_**kwargs/sandbox.py
+++ 27_day_tkinter_*args_**kwargs/sandbox.py
@@ -1,43 +1,60 @@
-# import tkinter
+from tkinter import *
 
-# window = tkinter.Tk()
-# window.title('My First Gui program')
-# window.minsize(width=500, height=300)
+window = Tk()
+window.title('My First Gui program')
+window.minsize(width=500, height=300)
 
-# my_label = tkinter.Label(text='I am a LAbel', font=('Arial', 24, 'bold'))
-# my_label.pack(side='right')
+# Label
+my_label = Label(text='I am a LAbel', font=('Arial', 24, 'bold'))
+my_label.pack()
 
-# window.mainloop()
+my_label['text'] = 'New text'
+my_label.config(text='New text')
 
-def add(*args):
-    summ_args = 0
-    for i in args:
-        summ_args += i
-    return summ_args
+# inputs
+gui_input = Entry(width=30)
+gui_input.pack()
+
+# Buttons
+
+def button_click():
+    my_label.config(text=gui_input.get())
+
+
+button = Button(text='Click me', command=button_click)
+button.pack()
+
+window.mainloop()
+
+# def add(*args):
+#     summ_args = 0
+#     for i in args:
+#         summ_args += i
+#     return summ_args
 
 # print(add(1,2,3))
 
-def calculate(n, **kwargs):
+# def calculate(n, **kwargs):
     # print(kwargs) # type of **kwargs is dict
     # for k, v in kwargs.items():
     #     print(k)
     #     print(v)
-    n += kwargs['add']
-    n *= kwargs['multiply']
+    # n += kwargs['add']
+    # n *= kwargs['multiply']
     # print(n)
 
 
-calculate(2, add=3, multiply=5)
+# calculate(2, add=3, multiply=5)
 
 
-class Car:
+# class Car:
 
-    def __init__(self, **kw):
-        self.make = kw.get('make')
-        self.model = kw.get('model')
-        self.color = kw.get('color')
-        self.seats = kw.get('seats')
+#     def __init__(self, **kw):
+#         self.make = kw.get('make')
+#         self.model = kw.get('model')
+#         self.color = kw.get('color')
+#         self.seats = kw.get('seats')
 
 
-my_car = Car(make='Nissan', model='GTR')
-print(my_car.model)
+# my_car = Car(make='Nissan', model='GTR')
+# print(my_car.model)
