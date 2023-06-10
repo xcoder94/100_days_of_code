1203934b9ec16edb32eb5d9a5479bdb4897a09b0
xcoder94 <o7451155@yandex.com>
2023-06-10 22:46:46 +0500

    day 29 started in 10.06.2023

---
+++ 29_day_password_manager/main.py
@@ -0,0 +1,59 @@
+# Modules
+from tkinter import *
+# ---------------------------- PASSWORD GENERATOR ------------------- #
+def geenerate_pass():
+    pass
+# ---------------------------- SAVE PASSWORD  ----------------------- #
+def save_pass():
+    pass
+# ---------------------------- UI SETUP ----------------------------- #
+
+window = Tk()
+window.title('Password Manager')
+window.config(padx=50, pady=50, bg='white')
+# window.minsize(width=400, height=220)
+
+canvas = Canvas(width=200, height=200, highlightthickness=0)
+lock_image = PhotoImage(file='logo.png')
+canvas.create_image(100, 100, image=lock_image) # first 100 is position in x axis
+canvas.grid(row=0, column=1)
+
+# User form 
+# Labels
+website_label = Label(text='Website:')
+website_label.grid(row=1, column=0)
+username_label = Label(text='Email/Username:')
+username_label.grid(row=2, column=0)
+password_label = Label(text='Password:')
+password_label.grid(row=3, column=0)
+
+# Inputs
+website_entry = Entry(width=35)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=35)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+# Buttons
+generate_btn = Button(text='Generate password', command=geenerate_pass, highlightthickness=0)
+generate_btn.grid(row=3, column=2)
+add_button = Button(text='Add', command=save_pass, width=36, highlightthickness=0)
+add_button.grid(row=4, column=1, columnspan=2)
+
+
+
+
+
+
+
+window.mainloop()
+
+
+
+
+
+
+
+
+
