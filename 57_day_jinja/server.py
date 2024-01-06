864b0eacd7ae65ce80d9b8fd0b8776f4328345d8
xcoder94 <o7451155@yandex.com>
2024-01-06 17:05:19 +0500

    day 57 started in 06.01.2023

---
+++ 57_day_jinja/server.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+import random
+from datetime import datetime
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    random_number = random.randint(1, 10)
+    current_year = datetime.now().year
+    return render_template('index.html', num=random_number, year=current_year)
+
+
+@app.route('/guess/<some_name>')
+def guess_page(some_name):
+    age_response = requests.get(f'https://api.agify.io?name={some_name}')
+    age_response.raise_for_status()
+    person_age = age_response.json()['age']
+    gender_response = requests.get(f'https://api.genderize.io?name={some_name}')
+    gender_response.raise_for_status()
+    person_gender = gender_response.json()['gender']
+    return render_template('guess.html', person_name=some_name, person_gender=person_gender, person_age=person_age)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
