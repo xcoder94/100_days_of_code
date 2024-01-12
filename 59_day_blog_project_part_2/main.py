4aabfcf84705088a33ef36238aa76dfcd5665482
xcoder94 <o7451155@yandex.com>
2024-01-12 18:13:59 +0500

    day 59 ended in 12.01.2024

---
+++ 59_day_blog_project_part_2/main.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template
+import requests
+from post import Post
+app = Flask(__name__)
+
+posts = requests.get('https://api.npoint.io/c790b4d5cab58020d391').json()
+post_objects = []
+for post in posts:
+    post_obj = Post(post['id'], post['title'], post['subtitle'], post['body'])
+    post_objects.append(post_obj)
+
+
+@app.route('/')
+def home_page():
+    return render_template('index.html', all_posts=post_objects)
+
+
+@app.route('/post/<int:index>')
+def show_post(index):
+    requested_post = None
+    for blog_post in post_objects:
+        if blog_post.id == index:
+            requested_post = blog_post
+    return render_template('post.html', post=requested_post)
+
+
+@app.route('/about')
+def about_page():
+    return render_template('about.html')
+
+
+@app.route('/contacts')
+def contacts_page():
+    return render_template('contact.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, host="localhost")
