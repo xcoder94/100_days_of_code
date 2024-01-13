49be1ed33d79c2d4cb5e1c7b36d88a88f35eb8da
xcoder94 <o7451155@yandex.com>
2024-01-13 16:01:19 +0500

    day 60 ended in 13.01.2024

---
+++ 60_day_flask_forms/main.py
@@ -0,0 +1,51 @@
+from flask import Flask, render_template, request
+import requests
+import smtplib
+
+my_email = 'orustamxon@gmail.com'
+password = 'qiwdjgqbjyelurjr'
+
+app = Flask(__name__)
+
+# USE YOUR OWN npoint LINK! ADD AN IMAGE URL FOR YOUR POST. 👇
+posts = requests.get("https://api.npoint.io/c790b4d5cab58020d391").json()
+
+
+@app.route('/')
+def get_all_posts():
+    return render_template("index.html", all_posts=posts)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+# SOLUTION to Challenge:
+@app.route("/contact", methods=["GET", "POST"])
+def contact():
+    if request.method == "POST":
+        data = request.form
+        with smtplib.SMTP('smtp.gmail.com') as connection:
+            connection.starttls()
+            connection.login(user=my_email, password=password)
+            connection.sendmail(
+                from_addr=my_email,
+                to_addrs=data['email'],
+                msg=data['message']
+            )
+        return render_template("contact.html", msg_sent=True)
+    return render_template("contact.html", msg_sent=False)
+
+
+@app.route("/post/<int:index>")
+def show_post(index):
+    requested_post = None
+    for blog_post in posts:
+        if blog_post["id"] == index:
+            requested_post = blog_post
+    return render_template("post.html", post=requested_post)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
