6336afe6a43cca6bd76c487a7f0dee463ac5e9f8
xcoder94 <o7451155@yandex.com>
2024-02-26 16:56:10 +0500

    Day 64 started in 21.02.2024

---
+++ 64_day_top_10_movies_website/main.py
@@ -0,0 +1,82 @@
+from flask import Flask, render_template, redirect, url_for, request
+from flask_bootstrap import Bootstrap5
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from sqlalchemy import Integer, String, Float
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
+import requests
+
+'''
+Red underlines? Install the required packages first: 
+Open the Terminal in PyCharm (bottom left). 
+
+On Windows type:
+python -m pip install -r requirements.txt
+
+On MacOS type:
+pip3 install -r requirements.txt
+
+This will install the packages from requirements.txt for this project.
+'''
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+Bootstrap5(app)
+
+
+# CREATE DB
+class Base(DeclarativeBase):
+    pass
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///movies_list.db"
+db = SQLAlchemy(model_class=Base)
+db.init_app(app)
+
+
+# CREATE TABLE
+class Movie(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    title: Mapped[str] = mapped_column(String(250), unique=True, nullable=False)
+    year: Mapped[int] = mapped_column(Integer, nullable=False)
+    description: Mapped[str] = mapped_column(String(500), nullable=False)
+    rating: Mapped[float] = mapped_column(Float, nullable=True)
+    ranking: Mapped[int] = mapped_column(Integer, nullable=True)
+    review: Mapped[str] = mapped_column(String(250), nullable=True)
+    img_url: Mapped[str] = mapped_column(String(250), nullable=False)
+
+    # Optional: this will allow each movie object to be identified by its title when printed.
+    def __repr__(self):
+        return f'<Movies {self.title}>'
+
+
+# Create table schema in the database. Requires application context.
+with app.app_context():
+    db.create_all()
+
+# with app.app_context():
+#     new_movie = Movie(
+#         title="Phone Booth",
+#         year=2002,
+#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller.",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
+#     )
+#     db.session.add(new_movie)
+#     db.session.commit()
+
+
+@app.route("/")
+def home():
+    with app.app_context():
+        result = db.session.execute(db.select(Movie).order_by(Movie.title))
+        movies_list = result.scalars().all()
+    return render_template("index.html", movies=movies_list)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
